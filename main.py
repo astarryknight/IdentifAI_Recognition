3e34c21df96a31a61480d9789e91da12d7dc9ac9
astarryknight <whiteh4tter@gmail.com>
2024-11-12 19:43:30 -0500

    testing http req

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import numpy as np
 from simple_facerec import SimpleFacerec
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv() 
 
 ip = os.getenv("IP")
+print(ip)
 
 #webcam:
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 #ESP32:
-#cap = cv2.VideoCapture(ip)
+cap = cv2.VideoCapture(ip)
 
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
 
-while True:
+while(not cap.isOpened()):
+    time.sleep(0.1)
+
+while(cap.isOpened()):
     ret, frame = cap.read()
 
     cv2.imshow("Frame", frame)
@@ -27,9 +32,9 @@ while True:
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
-    if name is not "Unkown":
-        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+    
+    cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
