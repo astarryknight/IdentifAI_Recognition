9f261b71b493158be537d584c0ee0e09721d60e1
astarryknight <whiteh4tter@gmail.com>
2024-11-12 22:30:06 -0500

    update gitignore and implement basic facial recognition through ESP32 camera server

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ print(ip)
 #webcam:
 #cap = cv2.VideoCapture(0)
 #ESP32:
-cap = cv2.VideoCapture(ip)
+#cap = cv2.VideoCapture(ip)
+
+cv2.namedWindow("test")
 
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
 
-while(not cap.isOpened()):
-    time.sleep(0.1)
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
+while True:
+    imgResponse = urllib.request.urlopen(ip, timeout=5)
+    imgNp = np.array(bytearray(imgResponse.read()), dtype=np.uint8)
+    frame=cv2.imdecode(imgNp, -1)
+    #cv2.imshow("test", img)
+    time.sleep(.2)
+    #ret, frame = cap.read()
 
     cv2.imshow("Frame", frame)
 
@@ -31,15 +35,14 @@ while(cap.isOpened()):
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-
-    
-    cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+        
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
     if key == 27:
         break
 
-cap.release()
+# cap.release()
 cv2.destroyAllWindows()
